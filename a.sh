72b395c821194ca43c4a3bcce9081c4379b547ec
marekklein <marek@gregoriana.sk>
2021-04-16 22:28:55 +0200

    FR

---
+++ a.sh
@@ -0,0 +1,11 @@
+lualatex --shell-escape fr05-feria5.tex
+lualatex --shell-escape fr06-feria6.tex
+lualatex --shell-escape fr06-feria6.tex
+lualatex --shell-escape fr07-sabbato.tex
+lualatex --shell-escape fr07-sabbato.tex
+lualatex --shell-escape fr08-resurrectionis.tex
+lualatex --shell-escape fr08-resurrectionis.tex
+lualatex --shell-escape fr09-minores-a.tex
+lualatex --shell-escape fr09-minores-a.tex
+lualatex --shell-escape fr10-minores-b.tex
+lualatex --shell-escape fr10-minores-b.tex
